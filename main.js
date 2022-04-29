57bbe029df254f658e56a8435031fe79e0e9e2d1
Kolja Nagel <kn685832@fh-muenster.de>
2022-04-29 11:29:05 +0200

    Added stat requests

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const cors = require('cors')
 const mysql = require('mysql');
 
 const interceptor = express.Router();
-const version = JSON.parse(fs.readFileSync('./package.json')).version
+const version = JSON.parse(fs.readFileSync('./package.json').toString()).version
 
 const pool = mysql.createPool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_DATABASE
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 });
 
 const app = express();
@@ -19,17 +19,17 @@ interceptor.use(onIntercept);
 app.use(cors()).use(express.json()).use(interceptor);
 
 app.post('/history', (req, res) => {
-	pool.query('SELECT DISTINCT trackid, contexturi, played_at FROM playback JOIN user ON userid = user.id WHERE sid = ? ORDER BY played_at DESC', [req.userId],
-		(error, results) => {
-			if (error) {
-				res.json(error).status(500).end();
-			}
-			res.json(results).end();
-		});
+    pool.query('SELECT DISTINCT trackid, contexturi, played_at FROM playback JOIN user ON userid = user.id WHERE sid = ? ORDER BY played_at DESC', [req.userId],
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
 });
 
 app.post('/topTrack', (req, res) => {
-	pool.query(`SELECT DISTINCT playback.trackid, c
+    pool.query(`SELECT DISTINCT playback.trackid, c
                 FROM playback
                          JOIN user ON userid = user.id
                          JOIN track t on playback.trackid = t.id
@@ -42,34 +42,34 @@ app.post('/topTrack', (req, res) => {
                 WHERE sid = ?
                 ORDER BY c DESC
                 LIMIT 50;`, [req.userId, req.userId],
-		(error, results) => {
-			if (error) {
-				res.json(error).status(500).end();
-			}
-			res.json(results).end();
-		});
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
 });
 
 app.post('/contextOfTrack', (req, res) => {
-	const {track_id} = req.body;
-	pool.query(`select distinct playback.trackid, playback.contexturi
+    const {track_id} = req.body;
+    pool.query(`select distinct playback.trackid, playback.contexturi
                 from playback
                          join user u on playback.userid = u.id
                 where sid = ?
                   and trackid = ?
                 order by trackid desc;`, [req.userId, track_id],
-		(error, results) => {
-			if (error) {
-				res.json(error).status(500).end();
-			}
-			res.json(results).end();
-		});
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
 });
 
 
 app.post('/listeningClock', (req, res) => {
-	const {from, to} = req.body;
-	pool.query(`select TIME_FORMAT(FROM_UNIXTIME(played_at), '%H') hour, count(*) as count
+    const {from, to} = req.body;
+    pool.query(`select TIME_FORMAT(FROM_UNIXTIME(played_at), '%H') hour, count(*) as count
                 from playback
                          join user u on playback.userid = u.id
                 where sid = ?
@@ -77,17 +77,17 @@ app.post('/listeningClock', (req, res) => {
                   and played_at <= ?
                 GROUP BY hour
                 order by hour ASC;`, [req.userId, from, to],
-		(error, results) => {
-			if (error) {
-				res.json(error).status(500).end();
-			}
-			res.json(results).end();
-		});
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
 });
 
 app.post('/streak', (req, res) => {
-	const {from, to} = req.body;
-	pool.query(`SELECT consec_set id, MAX(start) start, MAX(end) end, (count(consec_set) - 1) days
+    const {from, to} = req.body;
+    pool.query(`SELECT consec_set id, MAX(start) start, MAX(end) end, (count(consec_set) - 1) days
                 FROM (
                          SELECT IF(b.date IS NULL, @val := @val + 1, @val) AS consec_set,
                                 IF(b.date IS NULL, a.date, null)           AS start,
@@ -112,17 +112,17 @@ app.post('/streak', (req, res) => {
                      ) a
                 GROUP BY a.consec_set
                 ORDER BY days DESC;`, [req.userId, from, to, req.userId, from, to],
-		(error, results) => {
-			if (error) {
-				res.json(error).status(500).end();
-			}
-			res.json(results).end();
-		});
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
 });
 
 app.post('/mostActiveDay', (req, res) => {
-	const {from, to} = req.body;
-	pool.query(`select DATE_FORMAT(FROM_UNIXTIME(played_at), '%d.%m.%Y') date, count(*) count
+    const {from, to} = req.body;
+    pool.query(`select DATE_FORMAT(FROM_UNIXTIME(played_at), '%d.%m.%Y') date, count(*) count
                 from playback
                          join user u on playback.userid = u.id
                 where sid = ?
@@ -130,84 +130,120 @@ app.post('/mostActiveDay', (req, res) => {
                   and played_at <= ?
                 GROUP BY date
                 order by count DESC;`, [req.userId, from, to],
-		(error, results) => {
-			if (error) {
-				res.json(error).status(500).end();
-			}
-			res.json(results).end();
-		});
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
 });
 
 app.post('/contextPlayedCount', (req, res) => {
-	const {contextUri} = req.body;
-	pool.query(`select contexturi, count(*) count
+    const {contextUri} = req.body;
+    pool.query(`select contexturi, count(*) count
                 from playback
                          join user u on playback.userid = u.id
                 where sid = ?
                   AND contexturi = ?
                 GROUP BY contexturi
                 order by count DESC;`, [req.userId, contextUri],
-		(error, results) => {
-			if (error) {
-				res.json(error).status(500).end();
-			}
-			res.json(results).end();
-		});
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
 });
 
 app.post('/trackPlayedCount', (req, res) => {
-	const {trackId} = req.body;
-	pool.query(`select trackid, count(*) count
+    const {trackId} = req.body;
+    pool.query(`select trackid, count(*) count
                 from playback
                          join user u on playback.userid = u.id
                 where sid = ?
                   AND trackid = ?
                 GROUP BY trackid
                 order by count DESC;`, [req.userId, trackId],
-		(error, results) => {
-			if (error) {
-				res.json(error).status(500).end();
-			}
-			res.json(results).end();
-		});
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
 });
 
+app.post('/totalTracks', (req, res) => {
+    const {from, to} = req.body;
+    pool.query(`select count(*) count
+                from playback
+                         join user u on playback.userid = u.id
+                where sid = ?
+                  and played_at >= ?
+                  and played_at <= ?
+                order by count DESC;`, [req.userId, from, to],
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
+});
+
+app.post('/uniqueTracks', (req, res) => {
+    const {from, to} = req.body;
+    pool.query(`select count(DISTINCT(trackid)) count
+                from playback
+                         join user u on playback.userid = u.id
+                where sid = ?
+                  and played_at >= ?
+                  and played_at <= ?
+                order by count DESC;`, [req.userId, from, to],
+        (error, results) => {
+            if (error) {
+                res.json(error).status(500).end();
+            }
+            res.json(results).end();
+        });
+});
+
+
+
 app.get('/version', (req, res) => {
-	res.json({version: version}).end();
+    res.json({version: version}).end();
 });
 
 function onIntercept(req, res, next) {
-	if (req.url === '/version') {
-		next();
-	} else {
-		const {access_token} = req.body;
+    if (req.url === '/version') {
+        next();
+    } else {
+        const {access_token} = req.body;
 
-		https.get({
-			headers: {'Authorization': 'Bearer ' + access_token},
-			host: 'api.spotify.com',
-			port: 443,
-			path: '/v1/me/'
-		}, value => {
-			if (value.statusCode !== 200) {
-				res.status(401);
-				res.end()
-			} else {
-				let data = "";
-				value.on('data', chunk => {
-					data += String(chunk);
-				});
-				value.on('end', () => {
-					try {
-						req.userId = JSON.parse(String(data)).id;
-					} catch (e) {
-						console.log(e);
-						res.json(e.message).status(500).end();
-					}
-					next();
-				});
-			}
-		});
-	}
+        https.get({
+            headers: {'Authorization': 'Bearer ' + access_token},
+            host: 'api.spotify.com',
+            port: 443,
+            path: '/v1/me/'
+        }, value => {
+            if (value.statusCode !== 200) {
+                res.status(401);
+                res.end()
+            } else {
+                let data = "";
+                value.on('data', chunk => {
+                    data += String(chunk);
+                });
+                value.on('end', () => {
+                    try {
+                        req.userId = JSON.parse(String(data)).id;
+                    } catch (e) {
+                        console.log(e);
+                        res.json(e.message).status(500).end();
+                    }
+                    next();
+                });
+            }
+        });
+    }
 }
 
 app.listen(8096);
