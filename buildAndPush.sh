95018589a8045b000118e90e434e26bbe9d7b224
Kolja Nagel <kn685832@fh-muenster.de>
2022-08-14 16:12:22 +0200

    swagger update

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION=2.0.0
+VERSION=2.1.0
 docker build -t kojenka/spotify-playback-api:$VERSION .
-docker tag kojenka/spotify-playback-api:$VERSION kojenka/spotify-playback-api:latest
+docker tag kojenka/spotify-playback-api:$VERSION kojenka/spotify-playback-api:swagger-latest
 docker push kojenka/spotify-playback-api:$VERSION
-docker push kojenka/spotify-playback-api:latest
+docker push kojenka/spotify-playback-api:swagger-latest
